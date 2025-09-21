e44dfb562ab53bc0430a049c1846896b144d708f
Kevin Halley <73517606+keuwey@users.noreply.github.com>
2025-09-21 15:16:53 -0300

    Update code linting

--- guess_the_number/src/game.c
+++ guess_the_number/src/game.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(void)
-{
+int main(void) {
     srand(time(NULL));
     const unsigned short int NUMBER = rand() % 41;
     const unsigned short int user_guess;
@@ -11,31 +10,24 @@ int main(void)
 
     printf("\nI'm thinking of a number between 0 and 40.\n");
 
-    while (guesses_left > 0)
-    {
+    while (guesses_left > 0) {
         printf("You have %hu guess%s left.\n", guesses_left, guesses_left == 1 ? "" : "es");
         printf("Make a guess: ");
 
-        if (scanf("%hu", &user_guess) != 1)
-        {
+        if (scanf("%hu", &user_guess) != 1) {
             printf("Invalid input. Please enter a number.\n");
-            while (getchar() != '\n')
-                ;
-            continue;
+            while (getchar() != '\n') continue;
         }
 
         guesses_left--;
 
-        if (user_guess > NUMBER)
-        {
+        if (user_guess > NUMBER) {
             printf("Wrong! The number is lower than that.\n\n");
         }
-        else if (user_guess < NUMBER)
-        {
+        else if (user_guess < NUMBER) {
             printf("Wrong! The number is higher than that.\n\n");
         }
-        else
-        {
+        else {
             printf("\n🎉 Congratulations! You got it right!\n");
             printf("You guessed it in %hu attempt%s.\n\n", (5 - guesses_left), (5 - guesses_left == 1) ? "" : "s");
             return 0;
